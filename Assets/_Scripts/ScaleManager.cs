0afa24f2e8265afdbd77ffcffadba212905a0efe
ClayMStamper <ClayStamper@Gmail.com>
2018-09-22 16:18:16 -0500

    menu layed out

---
+++ Assets/_Scripts/ScaleManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleManager : MonoBehaviour {
+
+    [SerializeField]
+    List<Canvas> canvasList = new List<Canvas>();
+	
+}
+
+class ScaleElement {
+    
+}

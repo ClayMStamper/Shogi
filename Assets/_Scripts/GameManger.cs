22ff35e696dc89cbc516bfeebf345a4719c7c2c1
Not Clayton <claystamper@gmail.com>
2025-09-28 21:11:17 -0500

    stubbed in a pvp scene. I'm about to pull down Mirror as my replication framework, which is a community maintained plugin inspired by UNet

---
+++ Assets/_Scripts/GameManger.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace _Scripts {
+    public class GameManger : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
